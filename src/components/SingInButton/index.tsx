fee4e6c809372598b2082366de03bcff22a74c79
GiovanniCrabi <crabigiovanni@gmail.com>
2022-02-04 19:55:22 +0000

    Created signOn and singOut

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,21 +1,24 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss';
 
 
-
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const { data: session }= useSession();
 
-  return isUserLoggedIn ? (
+  console.log(session);
+
+  return session ? (
     (
       <button
        type="button"
        className={styles.singInButton}
+       onClick={() => signOut()}
       >
         <FaGithub color='#04d361'/>
-        Giovanni Crabi
+        {session.user.name}
         <FiX color='#737380' className={styles.closeIcon}/>
       </button>
     )
@@ -23,6 +26,7 @@ export function SingInButton() {
     <button
      type="button"
      className={styles.singInButton}
+     onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417'/>
       Sing with Github
