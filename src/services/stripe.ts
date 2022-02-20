10e0e808bec42d44e090ffbf849a336469885b65
GiovanniCrabi <crabigiovanni@gmail.com>
2022-02-20 16:56:15 +0000

    Created and Update webhooks

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,14 +1,12 @@
 import Stripe from 'stripe';
-import { version } from '../../package.json'
 
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
-    apiVersion: '2020-08-27',
+    apiVersion: null,
     appInfo: {
       name: 'Ignews',
-      version
     },
   }
 )
